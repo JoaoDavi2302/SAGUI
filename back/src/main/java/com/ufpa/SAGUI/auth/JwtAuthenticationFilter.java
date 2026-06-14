20c65f52bac9881f8f6c00ab505e33ecec1de2a5
JoaoDavi2302 <jd3870894@gmail.com>
2026-06-14 13:01:19 -0300

    fix: correção da estrutura do projeto e de imports

--- back/src/main/java/com/ufpa/SAGUI/auth/JwtAuthenticationFilter.java
+++ back/src/main/java/com/ufpa/SAGUI/auth/JwtAuthenticationFilter.java
@@ -1,28 +1,31 @@
 package com.ufpa.SAGUI.auth;
 
+import java.io.IOException;
+
+import com.ufpa.SAGUI.service.UserService;
+
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
-import com.ufpa.SAGUI.service.UserService;
-
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
-
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 
 @Component
 @RequiredArgsConstructor
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
-    private final JWTService jwtService;
+    private final JwtService jwtService;
     private final UserService userService;
 
     
     @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletReponse reponse,
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse reponse,
         FilterChain filterChain) throws IOException, ServletException{
             final String authHeader = request.getHeader("authorization");
 
--- back/src/main/java/com/ufpa/SAGUI/auth/JwtService.java
+++ back/src/main/java/com/ufpa/SAGUI/auth/JwtService.java
@@ -6,12 +6,15 @@ import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 
+import java.util.Date;
+
 import javax.crypto.SecretKey;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.stereotype.Service;
+
+import com.ufpa.SAGUI.models.User;
+
 
 @Service
 public class JwtService {
@@ -19,7 +22,7 @@ public class JwtService {
     private String jwtSecret;
     
     @Value("${jwt.expiration}")
-    private String jwtExpiration;
+    private Long jwtExpiration;
 
     // transforma jwt secret em chave criptográfica
     private SecretKey getSignInKey(){
@@ -32,13 +35,14 @@ public class JwtService {
         return Jwts.builder()
             .subject(user.getUsername())
             .claim("role", user.getRole().name())
-            .issuedAt(new Date(System.currentTimeMillis() + jwtExpiration))
+            .issuedAt(new Date(System.currentTimeMillis()))
+            .expiration(new Date(System.currentTimeMillis() + jwtExpiration))
             .signWith(getSignInKey())
             .compact();
     }
 
-    // extrai tokens
-    public String extractToken(String token){
+    // extrai email
+    public String extractEmail(String token){
         return Jwts.parser()
             .verifyWith(getSignInKey())
             .build()
@@ -49,16 +53,16 @@ public class JwtService {
 
     // validação de token
     public boolean isTokenValid(String token, UserDetails userDetails){
-        string email = extractToken(token);
+        String email = extractEmail(token);
 
-        return emails.equals(userDetails.getUsername()) && !isTokenExpired(token);
+        return email.equals(userDetails.getUsername()) && !isTokenExpired(token);
     }
 
     private boolean isTokenExpired(String token){
-        return extractToken(token).before(new Date());
+        return extractExpiration(token).before(new Date());
     }
 
-    private Date extractExpiration(string token){
+    private Date extractExpiration(String token){
         return Jwts.parser()
             .verifyWith(getSignInKey())
             .build()
