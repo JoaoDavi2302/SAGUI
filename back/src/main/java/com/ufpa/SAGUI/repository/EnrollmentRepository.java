2ac53553b091dc8d1240445c78c189edf1784fb1
woodysouza <woodysouza1@gmail.com>
2026-07-01 13:50:00 -0300

    Add files via upload
    gerencia de matriculas

---
+++ back/src/main/java/com/ufpa/SAGUI/repository/EnrollmentRepository.java
@@ -0,0 +1,15 @@
+
+package com.ufpa.SAGUI.repository;
+
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.ufpa.SAGUI.enums.EntityStatus;
+import com.ufpa.SAGUI.models.Enrollment;
+
+public interface EnrollmentRepository extends JpaRepository<Enrollment, UUID> {
+    
+    
+    boolean existsByStudent_IdAndDiscipline_IdAndStatusNot(UUID studentId, UUID disciplineId, EntityStatus status);
+}
