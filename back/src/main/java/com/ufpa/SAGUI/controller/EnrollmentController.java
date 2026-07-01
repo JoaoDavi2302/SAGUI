bb0f1875ff279fff26de22530dec75ed4437d474
woodysouza <woodysouza1@gmail.com>
2026-07-01 13:52:22 -0300

    Add files via upload

---
+++ back/src/main/java/com/ufpa/SAGUI/controller/EnrollmentController.java
@@ -0,0 +1,34 @@
+package com.ufpa.SAGUI.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ufpa.SAGUI.dto.enrollment.EnrollmentRequest;
+import com.ufpa.SAGUI.dto.enrollment.EnrollmentResponse;
+import com.ufpa.SAGUI.service.EnrollmentService;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/api/enrollments")
+@RequiredArgsConstructor
+public class EnrollmentController {
+
+    private final EnrollmentService enrollmentService;
+
+    
+    @PostMapping
+    public ResponseEntity<EnrollmentResponse> requestEnrollment(@Valid @RequestBody EnrollmentRequest request) {
+        
+        
+        EnrollmentResponse response = enrollmentService.requestEnrollment(request);
+        
+       
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+}
