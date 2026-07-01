44546478d1412e16a22aff9693c13fd3b6c48e1a
woodysouza <woodysouza1@gmail.com>
2026-07-01 14:02:16 -0300

    Create EnrollmentRequest.java

---
+++ back/src/main/java/com/ufpa/SAGUI/dto/enrollment/EnrollmentRequest.java
@@ -0,0 +1,17 @@
+package com.ufpa.SAGUI.dto.enrollment;
+
+import java.util.UUID;
+
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+
+@Data
+public class EnrollmentRequest {
+    @NotNull(message = "O ID do aluno é obrigatório")
+    private UUID studentId;
+
+    @NotNull(message = "O ID da disciplina é obrigatório")
+    private UUID disciplineId;
+
+    private UUID courseId; // Opcional, caso futuramente queira mapear por curso completo
+}
