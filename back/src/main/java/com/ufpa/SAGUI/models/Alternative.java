6e4442099810c37102645770e2f347e695188735
Heitor.v <heitor.versoza165@gmail.com>
2026-07-06 15:51:14 -0300

    Update Alternative.java

--- back/src/main/java/com/ufpa/SAGUI/models/Alternative.java
+++ back/src/main/java/com/ufpa/SAGUI/models/Alternative.java
@@ -1,1 +1,55 @@
+package com.ufpa.SAGUI.models;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "alternatives")
+public class Alternative extends BaseEntity {
+
+    @Column(nullable = false)
+    private String text;
+
+    @Column(nullable = false)
+    private Boolean correct;
+
+    @ManyToOne
+    @JoinColumn(name = "question_id", nullable = false)
+    private Question question;
+
+    public Alternative() {
+    }
+
+    public Alternative(String text, Boolean correct, Question question) {
+        this.text = text;
+        this.correct = correct;
+        this.question = question;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public Boolean getCorrect() {
+        return correct;
+    }
+
+    public void setCorrect(Boolean correct) {
+        this.correct = correct;
+    }
+
+    public Question getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(Question question) {
+        this.question = question;
+    }
+}
