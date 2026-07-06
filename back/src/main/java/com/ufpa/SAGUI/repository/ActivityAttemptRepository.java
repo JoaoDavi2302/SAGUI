c49fe22f885a360d0a554cbff31cf0792d3b9f0c
Heitor.v <heitor.versoza165@gmail.com>
2026-07-06 16:05:04 -0300

    Update ActivityAttemptRepository.java

--- back/src/main/java/com/ufpa/SAGUI/repository/ActivityAttemptRepository.java
+++ back/src/main/java/com/ufpa/SAGUI/repository/ActivityAttemptRepository.java
@@ -1,1 +1,14 @@
+package com.ufpa.SAGUI.repository;
 
+import com.ufpa.SAGUI.models.ActivityAttempt;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface ActivityAttemptRepository extends JpaRepository<ActivityAttempt, UUID> {
+
+    long countByStudentIdAndActivityId(UUID studentId, UUID activityId);
+
+    List<ActivityAttempt> findByStudentIdAndActivityId(UUID studentId, UUID activityId);
+}
