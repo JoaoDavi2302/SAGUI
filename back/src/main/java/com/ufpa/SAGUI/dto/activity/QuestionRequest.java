3c6c31878d1bcd47f68ab6877b564df44e4db988
Heitor.v <heitor.versoza165@gmail.com>
2026-07-06 16:10:35 -0300

    Update QuestionRequest.java

--- back/src/main/java/com/ufpa/SAGUI/dto/activity/QuestionRequest.java
+++ back/src/main/java/com/ufpa/SAGUI/dto/activity/QuestionRequest.java
@@ -1,1 +1,13 @@
+package com.ufpa.SAGUI.dto.activity;
 
+import com.ufpa.SAGUI.enums.QuestionType;
+
+import java.util.List;
+
+public record QuestionRequest(
+        String statement,
+        QuestionType questionType,
+        Double score,
+        List<AlternativeRequest> alternatives
+) {
+}
