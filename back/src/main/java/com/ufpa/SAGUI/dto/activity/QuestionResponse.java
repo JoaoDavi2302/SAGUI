433be5055b234a7e7e046cb674d3f49ca3546052
Heitor.v <heitor.versoza165@gmail.com>
2026-07-06 16:11:00 -0300

    Update QuestionResponse.java

--- back/src/main/java/com/ufpa/SAGUI/dto/activity/QuestionResponse.java
+++ back/src/main/java/com/ufpa/SAGUI/dto/activity/QuestionResponse.java
@@ -1,1 +1,15 @@
+package com.ufpa.SAGUI.dto.activity;
 
+import com.ufpa.SAGUI.enums.QuestionType;
+
+import java.util.List;
+import java.util.UUID;
+
+public record QuestionResponse(
+        UUID id,
+        String statement,
+        QuestionType questionType,
+        Double score,
+        List<AlternativeResponse> alternatives
+) {
+}
