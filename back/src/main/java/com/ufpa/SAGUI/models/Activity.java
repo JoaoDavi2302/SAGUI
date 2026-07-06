65e86484a1a37b6a66d5bc8520daf067e76f9bf0
Heitor.v <heitor.versoza165@gmail.com>
2026-07-06 15:46:02 -0300

    Update Activity.java

--- back/src/main/java/com/ufpa/SAGUI/models/Activity.java
+++ back/src/main/java/com/ufpa/SAGUI/models/Activity.java
@@ -1,1 +1,79 @@
+package com.ufpa.SAGUI.models;
 
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "activities")
+public class Activity extends BaseEntity {
+
+    @Column(nullable = false)
+    private String title;
+
+    private String description;
+
+    @Column(nullable = false)
+    private Integer attemptLimit = 3;
+
+    @Column(nullable = false)
+    private Double minimumScore = 70.0;
+
+    @OneToMany(mappedBy = "activity", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Question> questions = new ArrayList<>();
+
+    public Activity() {
+    }
+
+    public Activity(String title, String description) {
+        this.title = title;
+        this.description = description;
+        this.attemptLimit = 3;
+        this.minimumScore = 70.0;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Integer getAttemptLimit() {
+        return attemptLimit;
+    }
+
+    public void setAttemptLimit(Integer attemptLimit) {
+        this.attemptLimit = attemptLimit;
+    }
+
+    public Double getMinimumScore() {
+        return minimumScore;
+    }
+
+    public void setMinimumScore(Double minimumScore) {
+        this.minimumScore = minimumScore;
+    }
+
+    public List<Question> getQuestions() {
+        return questions;
+    }
+
+    public void setQuestions(List<Question> questions) {
+        this.questions = questions;
+    }
+}
