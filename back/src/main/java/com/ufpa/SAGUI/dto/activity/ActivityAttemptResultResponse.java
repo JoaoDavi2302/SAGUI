f77e35c1b569d8c7daee529d424ece94d54190a6
Heitor.v <heitor.versoza165@gmail.com>
2026-07-06 16:12:58 -0300

    Update ActivityAttemptResultResponse.java

--- back/src/main/java/com/ufpa/SAGUI/dto/activity/ActivityAttemptResultResponse.java
+++ back/src/main/java/com/ufpa/SAGUI/dto/activity/ActivityAttemptResultResponse.java
@@ -1,1 +1,12 @@
+package com.ufpa.SAGUI.dto.activity;
 
+import java.util.UUID;
+
+public record ActivityAttemptResultResponse(
+        UUID attemptId,
+        Integer attemptNumber,
+        Double score,
+        Boolean approved,
+        String message
+) {
+}
