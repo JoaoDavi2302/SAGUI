3e4f7334e4083fb9dea9f7fd3477707a29cc8094
Heitor.v <heitor.versoza165@gmail.com>
2026-07-06 17:35:34 -0300

    Update ActivityRequest.java

--- back/src/main/java/com/ufpa/SAGUI/dto/activity/ActivityRequest.java
+++ back/src/main/java/com/ufpa/SAGUI/dto/activity/ActivityRequest.java
@@ -1,8 +1,10 @@
 package com.ufpa.SAGUI.dto.activity;
 
 import java.util.List;
+import java.util.UUID;
 
 public record ActivityRequest(
+        UUID moduleId,
         String title,
         String description,
         List<QuestionRequest> questions
