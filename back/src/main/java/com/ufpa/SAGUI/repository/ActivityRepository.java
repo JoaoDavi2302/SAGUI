4cb53cd024ac3a8b26053cafcf5a874c2fd96e96
Heitor.v <heitor.versoza165@gmail.com>
2026-07-06 16:01:30 -0300

    Update ActivityRepository.java

--- back/src/main/java/com/ufpa/SAGUI/repository/ActivityRepository.java
+++ back/src/main/java/com/ufpa/SAGUI/repository/ActivityRepository.java
@@ -1,1 +1,9 @@
+package com.ufpa.SAGUI.repository;
 
+import com.ufpa.SAGUI.models.Activity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface ActivityRepository extends JpaRepository<Activity, UUID> {
+}
