8339ef64c460238e87b7052c6cf7585531eeea39
Heitor.v <heitor.versoza165@gmail.com>
2026-07-06 16:12:10 -0300

    Update StudentAnswerRequest.java

--- back/src/main/java/com/ufpa/SAGUI/dto/activity/StudentAnswerRequest.java
+++ back/src/main/java/com/ufpa/SAGUI/dto/activity/StudentAnswerRequest.java
@@ -1,1 +1,10 @@
+package com.ufpa.SAGUI.dto.activity;
 
+import java.util.List;
+import java.util.UUID;
+
+public record StudentAnswerRequest(
+        UUID questionId,
+        List<UUID> selectedAlternativeIds
+) {
+}
